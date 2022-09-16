f86c228a1fa94e73f177c40cd18f645af007a880
Muhammad Shaheer <shaheer.chry.com>
2022-09-17 02:10:04 +0500

    Linters

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,49 +1,49 @@
 require 'rspec'
-require './Fizz_Buzz'
-describe 'Fizz_Buzz' do
+require './fizz_buzz'
+describe 'fizz_buzz' do
   before(:each) do
-    @Fizz_Buzz = Solver.new
+    @fizz_buzz = Solver.new
   end
   context '#factorial' do
     it 'should return 120 for factorial 5' do
-      expect(@Fizz_Buzz.factorial(5)).to eq 120
+      expect(@fizz_buzz.factorial(5)).to eq 120
     end
     it 'should return 1 for factorial 0' do
-      expect(@Fizz_Buzz.factorial(0)).to eq 1
+      expect(@fizz_buzz.factorial(0)).to eq 1
     end
     it 'should return 1 for factorial of 1' do
-      expect(@Fizz_Buzz.factorial(1)).to eq 1
+      expect(@fizz_buzz.factorial(1)).to eq 1
     end
     it 'should return 2 for factorial of 2' do
-      expect(@Fizz_Buzz.factorial(2)).to eq 2
+      expect(@fizz_buzz.factorial(2)).to eq 2
     end
     it 'should return "Number Is Negative" for factorial -1' do
-      expect(@Fizz_Buzz.factorial(-1)).to eq 'Number Is Negative'
+      expect(@fizz_buzz.factorial(-1)).to eq 'Number Is Negative'
     end
   end
   context 'Reverse' do
     it 'should return "olleh" for "hello"' do
-      expect(@Fizz_Buzz.reverse('hello')).to eq 'olleh'
+      expect(@fizz_buzz.reverse('hello')).to eq 'olleh'
     end
     it 'should return "error" for "error"' do
-      expect(@Fizz_Buzz.reverse(1)).to eq 'Error'
+      expect(@fizz_buzz.reverse(1)).to eq 'Error'
     end
   end
   context 'fizzbuzz' do
     it 'should return "Fizz" for 3' do
-      expect(@Fizz_Buzz.fizzbuzz(3)).to eq 'Fizz'
+      expect(@fizz_buzz.fizzbuzz(3)).to eq 'Fizz'
     end
     it 'should return "Buzz" for 5' do
-      expect(@Fizz_Buzz.fizzbuzz(5)).to eq 'Buzz'
+      expect(@fizz_buzz.fizzbuzz(5)).to eq 'Buzz'
     end
     it 'should return "FizzBuzz" for 15' do
-      expect(@Fizz_Buzz.fizzbuzz(15)).to eq 'FizzBuzz'
+      expect(@fizz_buzz.fizzbuzz(15)).to eq 'FizzBuzz'
     end
     it 'should return "Fizz" for 6' do
-      expect(@Fizz_Buzz.fizzbuzz(6)).to eq 'Fizz'
+      expect(@fizz_buzz.fizzbuzz(6)).to eq 'Fizz'
     end
     it 'should return "7" for 7' do
-      expect(@Fizz_Buzz.fizzbuzz(7)).to eq '7'
+      expect(@fizz_buzz.fizzbuzz(7)).to eq '7'
     end
   end
 end
