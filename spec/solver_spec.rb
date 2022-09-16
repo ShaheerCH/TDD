b6e25e15fd416ab94bdcdeff9ce4cc5f4c1cfa33
Muhammad Shaheer <shaheer.chry.com>
2022-09-17 02:17:58 +0500

    Edit

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,8 +5,8 @@ describe 'fizz_buzz' do
     @fizz_buzz = Solver.new
   end
   context '#factorial' do
-    it 'should return 120 for factorial 5' do
-      expect(@fizz_buzz.factorial(5)).to eq 120
+    it 'should return 720 for factorial 6' do
+      expect(@fizz_buzz.factorial(6)).to eq 720
     end
     it 'should return 1 for factorial 0' do
       expect(@fizz_buzz.factorial(0)).to eq 1
@@ -17,16 +17,20 @@ describe 'fizz_buzz' do
     it 'should return 2 for factorial of 2' do
       expect(@fizz_buzz.factorial(2)).to eq 2
     end
-    it 'should return "Number Is Negative" for factorial -1' do
-      expect(@fizz_buzz.factorial(-1)).to eq 'Number Is Negative'
+    it 'should return "Negative Number" for factorial -1' do
+      expect(@fizz_buzz.factorial(-4)).to eq 'Negative Number'
     end
   end
   context 'Reverse' do
     it 'should return "olleh" for "hello"' do
       expect(@fizz_buzz.reverse('hello')).to eq 'olleh'
     end
+  context 'Reverse' do
+    it  'should return "hannah" for "hannah"' do
+      expect(@fizz_buzz.reverse('hannah')).to eq 'hannah'
+      end  
     it 'should return "error" for "error"' do
-      expect(@fizz_buzz.reverse(1)).to eq 'Error'
+      expect(@fizz_buzz.reverse(2)).to eq 'Error'
     end
   end
   context 'fizzbuzz' do
@@ -39,8 +43,8 @@ describe 'fizz_buzz' do
     it 'should return "FizzBuzz" for 15' do
       expect(@fizz_buzz.fizzbuzz(15)).to eq 'FizzBuzz'
     end
-    it 'should return "Fizz" for 6' do
-      expect(@fizz_buzz.fizzbuzz(6)).to eq 'Fizz'
+    it 'should return "Fizz" for 9' do
+      expect(@fizz_buzz.fizzbuzz(9)).to eq 'Fizz'
     end
     it 'should return "7" for 7' do
       expect(@fizz_buzz.fizzbuzz(7)).to eq '7'
